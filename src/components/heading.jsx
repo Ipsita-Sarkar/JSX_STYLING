20200b0f30e272c435e59bde05023e42c5f7ca99
sheerangel <77532755+sheerangel@users.noreply.github.com>
2021-01-17 20:29:24 +0530

    Components created

---
+++ src/components/heading.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+function Heading() {
+  return <h1 className="heading">My Favourite Foods</h1>;
+}
+export default Heading;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-ReactDOM.render(
-  <div>
-    <h1 className="heading">My Favourite Foods</h1>
-    <ul>
-      <li>Rice</li>
-      <li>Pizza</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+import App from "./components/app";
+ReactDOM.render(<App />, document.getElementById("root"));
